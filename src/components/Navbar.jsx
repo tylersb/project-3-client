f103aad0da154ce3d32829e797ede07ee07c59f8
charleslamb212 <charlesandrewlamb@gmail.com>
2023-01-29 15:44:09 -0800

    added appbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,33 @@
 import { Link } from 'react-router-dom'
-
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import index from '../index.css';
 export default function Navbar({ currentUser, handleLogout }) {
   const loggedIn = (
     <>
+	
       {/* if the user is logged in... */}
-      <Link to="/profile">
-        <p>Profile</p>
-      </Link>
+	  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+			<Link to="/profile">
+				<p>Profile</p>
+			</Link>
 
-      <Link to="/checkout">
-        <p>Checkout</p>
-      </Link>
+			<Link to="/checkout">
+				<p>Checkout</p>
+			</Link>
 
-      <Link to="/">
-        <p>
-          <span onClick={handleLogout}>Logout</span>
-        </p>
-      </Link>
+			<Link to="/">
+				<p>
+					<span onClick={handleLogout}>Logout</span>
+				</p>
+			</Link>
+			</Typography>
+	
     </>
   )
 
@@ -30,16 +41,24 @@ export default function Navbar({ currentUser, handleLogout }) {
   )
 
   return (
-    <nav>
-      {/* user always sees this section */}
-      <Link to="/">
-        <p>Home</p>
-      </Link>
+    <nav className='navbar'>
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					{/* user always sees this section */}
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						<Link to="/">
+							<p>Home</p>
+						</Link>
 
-	  <Link to="/menu">
-		<p>Menu</p>
-	  </Link>
-      {currentUser ? loggedIn : loggedOut}
+						<Link to="/menu">
+							<p>Menu</p>
+						</Link>
+					</Typography>
+					{currentUser ? loggedIn : loggedOut}
+				</Toolbar>
+			</AppBar>
+		</Box>
     </nav>
   )
 }
