9e58dda9fda41c47c3825e90ae5166580c927fc9
tylersb <tylersb@gmail.com>
2023-01-29 17:38:22 -0800

    finishes review components with working connections to database and ability to update

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-// create function component Menu
+import Reviews from '../Reviews'
 
+// create function component Menu
 export default function Menu(props) {
   // state is menuItems, setMenuItems is the function to update state, same applies to selectedItem, and cart
   const [selectedItem, setSelectedItem] = useState(null)
@@ -36,6 +37,7 @@ export default function Menu(props) {
         {menu}
       </div>
       {selectedItem && <p>Selected: {selectedItem.name}</p>}
+      <Reviews restaurantId={props.restaurant._id} currentUser={props.currentUser} />
     </div>
   )
   // if selectedItem is not null, display the name of the selected item, && is a conditional operator that checks if the first value is true, if it is, it displays the second value
