7c9f55ce559e84346947371a19a75875ccd125e7
tylersb <tylersb@gmail.com>
2023-01-30 11:32:03 -0800

    adds mui shopping cart buttons to menu

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState } from 'react'
 import Reviews from '../Reviews'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+import IconButton from '@mui/material/IconButton'
 
 // create function component Menu
 export default function Menu(props) {
   // state is menuItems, setMenuItems is the function to update state, same applies to selectedItem, and cart
   const [selectedItem, setSelectedItem] = useState(null)
-
 
   const handleSelection = (item) => {
     setSelectedItem(item) // updates the state of selectedItem with the item that was clicked
@@ -19,11 +19,22 @@ export default function Menu(props) {
         <div>
           {section.products.map((item) => (
             <div key={item._id}>
-              <button onClick={() => handleSelection(item)}>{item.name}</button>
-              <button onClick={() => props.handleAddToCart({
-                name: item.name,
-                price: item.price
-              })}>Add to Cart</button>
+              <span
+                onClick={() => handleSelection(item)}
+                style={{ cursor: 'pointer' }}
+              >
+                {item.name} - ${item.price}
+              </span>
+              <IconButton color="primary" aria-label="add to shopping cart"
+              onClick={() =>
+                  props.handleAddToCart({
+                    name: item.name,
+                    price: item.price
+                  })
+                }
+              >
+                <AddShoppingCartIcon />
+              </IconButton>
             </div>
           ))}
         </div>
@@ -34,11 +45,12 @@ export default function Menu(props) {
   return (
     <div>
       <h1>Menu</h1>
-      <div>
-        {menu}
-      </div>
+      <div>{menu}</div>
       {selectedItem && <p>Selected: {selectedItem.name}</p>}
-      <Reviews restaurantId={props.restaurant._id} currentUser={props.currentUser} />
+      <Reviews
+        restaurantId={props.restaurant._id}
+        currentUser={props.currentUser}
+      />
     </div>
   )
   // if selectedItem is not null, display the name of the selected item, && is a conditional operator that checks if the first value is true, if it is, it displays the second value
