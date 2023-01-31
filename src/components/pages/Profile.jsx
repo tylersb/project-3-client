c0e105f2fdbb0997085da17f4cbeeead4cb0787d
charleslamb212 <charlesandrewlamb@gmail.com>
2023-01-31 11:38:24 -0800

    profile

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,14 +1,20 @@
+import Typography from '@mui/material/Typography';
 import OrderHistory from '../OrderHistory'
 
 export default function Profile({ currentUser, handleLogout }) {
   return (
     <div>
+      <Typography>
       <h1>Profile</h1>
+      </Typography>
+      <Typography>
       <p>Username: {currentUser?.name}</p>
       <p>Email: {currentUser?.email}</p>
       <p>
         Address: {currentUser?.address.street}, {currentUser?.address.city}, {currentUser?.address.state} {currentUser?.address.zip}
       </p>
+      </Typography>
+      
       <OrderHistory userId={currentUser?.id} />
     </div>
   )
