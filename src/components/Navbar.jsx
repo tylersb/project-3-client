a48ba2e0cc3829e069513353752c417274544542
tylersb <tylersb@gmail.com>
2023-01-31 17:31:17 -0800

    visual redesign for the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,84 +1,213 @@
-import { Link } from 'react-router-dom'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
+import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
+import Tooltip from '@mui/material/Tooltip'
+import MenuItem from '@mui/material/MenuItem'
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined'
 import SubHeaderNavigation from './SubHeaderNavigation'
-import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
-
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Navbar({ currentUser, handleLogout }) {
-  const loggedIn = (
-    <>
-      {/* if the user is logged in... */}
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        <Button variant="contained" color="inherit">
-          <Link to="/profile">
-            <p>Profile</p>
-          </Link>
-        </Button>
-        <Button variant="contained" color="inherit">
-          <Link to="/checkout">
-            <p>Checkout</p>
-          </Link>
-        </Button>
-        <Button variant="contained" color="inherit">
-          <Link to="/">
-            <p>
-              <span onClick={handleLogout}>Logout</span>
-            </p>
-          </Link>
-        </Button>
+  const [anchorElNav, setAnchorElNav] = useState(null)
+  const [anchorElUser, setAnchorElUser] = useState(null)
 
-			{/* shopping cart button */}
-			<Link to='/checkout' >
-				<ShoppingBagOutlinedIcon
-					fontSize='large'
-					color='primary'
-				/>
-			</Link>
-      </Typography>
-    </>
-  )
+  // functions for opening and closing nav and user menus
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget)
+  }
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget)
+  }
 
-  const loggedOut = (
-    <>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        <Button variant="contained" color="inherit">
-          {/* if the user is not logged in... */}
-          <Link to="/register">Register</Link>
-        </Button>
-        <Button variant="contained" color="inherit">
-          <Link to="/login">Login</Link>
-        </Button>
-      </Typography>
-    </>
-  )
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null)
+  }
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null)
+  }
+
+  // array of pages for nav menu
+  const pages = [
+    {
+      name: 'Menu',
+      link: '/menu'
+    },
+    {
+      name: 'Checkout',
+      link: '/checkout'
+    }
+  ]
+
+  // condtiional rendering for user menu
+  let settings = []
+  currentUser
+    ? (settings = [
+        {
+          name: 'Profile',
+          link: '/profile',
+          click: handleCloseUserMenu
+        },
+        {
+          name: 'Logout',
+          link: '/',
+          click: handleLogout
+        }
+      ])
+    : (settings = [
+        {
+          name: 'Login',
+          link: '/login',
+          click: handleCloseUserMenu
+        },
+        {
+          name: 'Register',
+          link: '/register',
+          click: handleCloseUserMenu
+        }
+      ])
 
   return (
-    <nav className="navbar">
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            {/* user always sees this section */}
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Button variant="contained" color="inherit">
-                <Link to="/">
-                  <p>Home</p>
-                </Link>
-              </Button>
-              <Button variant="contained" color="inherit">
-                <Link to="/menu">
-                  <p>Menu</p>
-                </Link>
-              </Button>
+    <nav>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            {' '}
+            <Typography
+              variant="h6"
+              noWrap
+              component={Link}
+              to="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none'
+              }}
+            >
+              axios.get(food)
             </Typography>
-            {currentUser ? loggedIn : loggedOut}
-      <SubHeaderNavigation />
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left'
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left'
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' }
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem
+                    component={Link}
+                    to={page.link}
+                    key={page.name}
+                    onClick={handleCloseNavMenu}
+                  >
+                    {page.name}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h5"
+              noWrap
+              component={Link}
+              to="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none'
+              }}
+            >
+              axios.get(food)
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page.name}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                  component={Link}
+                  to={page.link}
+                >
+                  {page.name}
+                </Button>
+              ))}
+            </Box>
+            <SubHeaderNavigation />
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem
+                    key={setting.name}
+                    onClick={setting.click}
+                    component={Link}
+                    to={setting.link}
+                  >
+                    <Typography textAlign="center">{setting.name}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
           </Toolbar>
-        </AppBar>
-      </Box>
+        </Container>
+      </AppBar>
     </nav>
   )
 }
