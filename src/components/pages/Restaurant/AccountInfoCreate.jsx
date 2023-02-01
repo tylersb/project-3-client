27adbb18de3bbc891a1a7a5028d80348dbd4d712
tylersb <tylersb@gmail.com>
2023-02-01 12:40:15 -0800

    RegisterRestaurant and AccountInfoCreate components refactored

--- src/components/pages/Restaurant/AccountInfoCreate.jsx
+++ src/components/pages/Restaurant/AccountInfoCreate.jsx
@@ -1,118 +1,128 @@
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import { useState } from 'react';
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
 
-function AccountInfoCreate({handleAddAddress, msg, email, setEmail, phone, setPhone, accountHolderName, setAccountHolderName, restaurantName, setRestaurantName, restaurantDescription, setRestaurantDescription}) {
-	const [address, setAddress] = useState({
-		street: '',
-		city: '',
-		state: '',
-		zip: ''
-	})
-	return ( 
-        <Box component="div" sx={{ p: 2, m: 20, border: '1px dashed grey', width: '35%' }}>
-			<p>{msg}</p>
+export default function AccountInfoCreate({
+  restaurantInfo,
+  setRestaurantInfo,
+  setAccountInfoComplete
+}) {
 
-			<h1>Restaurant Account Information:</h1>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setAccountInfoComplete(true)
+  }
 
-			<div style={{ width: '80%', margin: '0 auto' }}>
-				<label htmlFor='restaurantName'>Restaurant Name:</label>
-				<TextField 
-					type="text"
-					variant="filled"
-					id="restaurantName"
-					placeholder='Pizza Galore'
-					onChange={e => setRestaurantName(e.target.value)}
-					value={restaurantName}
-				/>
+  return (
+    <Box
+      component="div"
+      sx={{ p: 2, m: 20, border: '1px dashed grey', width: '35%' }}
+    >
+      <h1>Restaurant Account Information:</h1>
 
-				<label htmlFor='accountHolderName'>Account Holder Name:</label>
-				<TextField 
-					type="text"
-					variant="filled"
-					id="accountHolderName"
-					placeholder='Mr. Pizza'
-					onChange={e => setAccountHolderName(e.target.value)}
-					value={accountHolderName}
-				/>
-				<label htmlFor='restaurantDescription'>Restaurant Description:</label>
-				<TextField 
-					type="text"
-					variant="filled"
-					id="restaurantDescription"
-					multiline
-					rows={4}
-					placeholder='A real Italian Pizzeria with only the bestest, freshest ingredients'
-					onChange={e => setRestaurantDescription(e.target.value)}
-					value={restaurantDescription}
-				/>
+      <div style={{ width: '80%', margin: '0 auto' }}>
+        <form
+          onSubmit={handleSubmit}
+        >
+        <label htmlFor="restaurantName">Restaurant Name:</label>
+        <TextField
+          type="text"
+          variant="filled"
+          id="restaurantName"
+          placeholder="Pizza Galore"
+          onChange={(e) =>
+            setRestaurantInfo({
+              ...restaurantInfo,
+              restaurantName: e.target.value
+            })
+          }
+          value={restaurantInfo.restaurantName}
+        />
 
-				<label htmlFor='email'>Email:</label>
-				<TextField  
-					type="email"
-					id="email"
-					variant="filled"
-					placeholder='your email...'
-					onChange={e => setEmail(e.target.value)}
-					value={email}
-				/>
-				<label htmlFor='phone'>Phone:</label>
-				<TextField  
-					type="Number"
-					id="phone"
-					variant="filled"
-					placeholder='your phone...'
-					onChange={e => setPhone(e.target.value)}
-					value={phone}
-				/>
-				<h4>Restaurant Address</h4>
-				<label htmlFor='street'>Street:</label>
-				<TextField  
-					type="text"
-					id="street"
-					variant="filled"
-					placeholder='123 Street...' 
-					onChange={e => setAddress({...address, street: e.target.value})}
-					value={address.street}
-				/>
-				<label htmlFor='city'>City:</label>
-				<TextField  
-					type="text"
-					id="city"
-					variant="filled"
-					placeholder='Los Angeles'
-					onChange={e => setAddress({...address, city: e.target.value})}
-					value={address.city}
-				/>
-				<label htmlFor='state'>State:</label>
-				<TextField  
-					type="text"
-					id="state"
-					variant="filled"
-					placeholder='California'
-					onChange={e => setAddress({...address, state: e.target.value})}
-					value={address.state}
-				/>
-				<label htmlFor='zip'>Zip Code:</label>
-				<TextField  
-					type="number"
-					id="zip"
-					variant="filled"
-					placeholder='12345'
-					onChange={e => setAddress({...address, zip: e.target.value})}
-					value={address.zip}
-				/>
-				<Button 
-					variant="outlined" 
-					type="submit" 
-					onClick={() => handleAddAddress(address)}>
-					Continue to Menu
-				</Button>
-			</div>
-		</Box>
-     );
+        <label htmlFor="accountHolderName">Account Holder Name:</label>
+        <TextField
+          type="text"
+          variant="filled"
+          id="accountHolderName"
+          placeholder="Mr. Pizza"
+          onChange={(e) => setRestaurantInfo({ ...restaurantInfo, accountHolderName: e.target.value })}
+          value={restaurantInfo.accountHolderName}
+        />
+        <label htmlFor="restaurantDescription">Restaurant Description:</label>
+        <TextField
+          type="text"
+          variant="filled"
+          id="restaurantDescription"
+          multiline
+          rows={4}
+          placeholder="A real Italian Pizzeria with only the bestest, freshest ingredients"
+          onChange={(e) => setRestaurantInfo({ ...restaurantInfo, restaurantDescription: e.target.value })}
+          value={restaurantInfo.restaurantDescription}
+        />
+
+        <label htmlFor="email">Email:</label>
+        <TextField
+          type="email"
+          id="email"
+          variant="filled"
+          placeholder="your email..."
+          onChange={(e) => setRestaurantInfo({ ...restaurantInfo, email: e.target.value })}
+          value={restaurantInfo.email}
+        />
+        <label htmlFor="phone">Phone:</label>
+        <TextField
+          type="Number"
+          id="phone"
+          variant="filled"
+          placeholder="your phone..."
+          onChange={(e) => setRestaurantInfo({ ...restaurantInfo, phone: e.target.value })}
+          value={restaurantInfo.phone}
+        />
+        <h4>Restaurant Address</h4>
+        <label htmlFor="street">Street:</label>
+        <TextField
+          type="text"
+          id="street"
+          variant="filled"
+          placeholder="123 Street..."
+          onChange={(e) => setRestaurantInfo({ ...restaurantInfo, street: e.target.value })}
+          value={restaurantInfo.street}
+        />
+        <label htmlFor="city">City:</label>
+        <TextField
+          type="text"
+          id="city"
+          variant="filled"
+          placeholder="Los Angeles"
+          onChange={(e) => setRestaurantInfo({ ...restaurantInfo, city: e.target.value })}
+          value={restaurantInfo.city}
+        />
+        <label htmlFor="state">State:</label>
+        <TextField
+          type="text"
+          id="state"
+          variant="filled"
+          placeholder="California"
+          onChange={(e) => setRestaurantInfo({ ...restaurantInfo, state: e.target.value })}
+          value={restaurantInfo.state}
+        />
+        <label htmlFor="zip">Zip Code:</label>
+        <TextField
+          type="number"
+          id="zip"
+          variant="filled"
+          placeholder="12345"
+          onChange={(e) => setRestaurantInfo({ ...restaurantInfo, zip: e.target.value })}
+          value={restaurantInfo.zip}
+        />
+        <Button
+          variant="outlined"
+          type="submit"
+        >
+          Continue to Menu
+        </Button>
+        </form>
+      </div>
+    </Box>
+  )
 }
-
-export default AccountInfoCreate;
